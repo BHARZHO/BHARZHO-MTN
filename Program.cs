32e6295fc89fdd3d2ab556d4650dc6b23e5a50ba
Abdulsalam Abdulbasit <abdulbasitabdulsalam@gmail.com>
2023-09-21 02:06:08 -0700

    my mtn suscription app

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Console.Write("When will your subscription expire: ");
+
+// int daysLeft = Convert.ToInt32(Console.ReadLine());
+
+// // switch (daysLeft){
+// //     case 5:
+// //     
+// //     break;
+
+    
+// // }
+// if (daysLeft >=1)
+// {
+//  if (daysLeft == 5){
+//     Console.WriteLine("Your subscription will expire in 5 days you have been given a discount of 20% on ypur roll over");
+// }
+// else if (daysLeft <= 4){
+//     Console.WriteLine("Your subscription will expire soon");
+// }
+// if (daysLeft == 1) {
+//     Console.WriteLine("Your subscribtion is going to expire tommorow and  we will like to give you a late 10% awuf");
+// }   
+// }
+// else
+// {
+//     System.Console.WriteLine("Oga you don write nonesence shey nah sporty bet you dey play");
+// }
+
+
+
+
+// Build a dice game
+ 
